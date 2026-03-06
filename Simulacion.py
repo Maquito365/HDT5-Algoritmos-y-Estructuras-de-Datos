a776e02be5723fc6828e905c2a2e03ca57f7e6e3
Maquito365 <macsoloj@gmail.com>
2026-03-05 19:04:06 -0600

    Se hizo el constructor de la clase proceso con lo necesario apra el proceso del programa

--- Simulacion.py
+++ Simulacion.py
@@ -5,7 +5,13 @@ import matplotlib.pyplot as plt
 
 # Definición de la clase Proceso y Simulacion
 class Proceso:
-    pass
-
+#Constructor de la clase Proceso
+    def __init__(self, id_proceso, tiempo_llegada):
+        self.id = id_proceso #se guarda el id
+        self.memoria =random.randint(1, 10)
+        self.instrucciones_totales = random.randint(1,10)
+        self.instrucciones_restantes = self.instrucciones_totales
+        self.tiempo_llegada = tiempo_llegada
+        self.tiempo_salida = 0
 class Simulacion:
     pass
