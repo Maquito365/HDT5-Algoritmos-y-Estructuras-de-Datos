df970f503ee9d850dd6c0afb21e15fac97f64aa6
Maquito365 <macsoloj@gmail.com>
2026-03-05 18:39:46 -0600

    Creación de las clases a utilizar en el programa

---
+++ Simulacion.py
@@ -0,0 +1,11 @@
+import simpy
+import random
+import statistics
+import matplotlib.pyplot as plt
+
+# Definición de la clase Proceso y Simulacion
+class Proceso:
+    pass
+
+class Simulacion:
+    pass
